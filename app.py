85ab6a766c97a1dbe5b0eb18fa10bc080dff1238
Dave Patel <divy@Daves-MacBook-Air.local>
2025-05-12 23:20:52 -0400

    Switched from pyjokes to JokeAPI

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-import pyjokes
+import requests
 
 app = Flask(__name__)
 
@@ -9,7 +9,10 @@ def home():
 
 @app.route('/get_joke', methods=["POST"])
 def get_joke():
-    joke = pyjokes.get_joke()
+    # Request a single-part programming joke from JokeAPI
+    response = requests.get("https://v2.jokeapi.dev/joke/Programming?type=single")
+    data = response.json()
+    joke = data.get("joke", "Oops! No joke found.")
     return jsonify(joke=joke)
 
 if __name__ == '__main__':
